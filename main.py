025bef81faa71d16779cb425136b548c959aa8c3
cozzi47 <cozzi47@hotmail.com>
2025-01-25 19:32:15 -0500

    D: add letter count report function

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     character_count = get_character_count(text)
-    print(character_count)
-
+    letter_report = get_letter_report(book_path, word_count, character_count)
+    
 
 def get_book_text(path):
     with open(path) as f:
@@ -25,6 +25,27 @@ def get_character_count(text):
         else:
             count[c] += 1
     return count
+    
+
+#def sort_on(character_count):
+    return character_count["count"]
+    
+    
+def get_letter_report(book_path, word_count, character_count):
+    letters = []
+    for character in character_count:
+        count = character_count[character]
+        if character.isalpha():
+            letters.append((character, count))
+    letters.sort(reverse=True, key=lambda x: x[1])   
+    
+    print (f"--- Begin report of {book_path} ---")  
+    print (f"{word_count} words found in the document")
+    print ()
+    for character, count in letters:
+        print(f"The '{character}' character was found {count} times")
+    print(f"--- End report ---")
+    
     
 
 
