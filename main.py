2264103721aa512a9c57603a8be675ea049265e3
cozzi47 <cozzi47@hotmail.com>
2025-02-22 14:30:27 -0500

    add stats.py, dynamic book_path in main.py

--- main.py
+++ main.py
@@ -1,48 +1,45 @@
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
-    word_count = get_word_count(text)
-    character_count = get_character_count(text)
-    letter_report = get_letter_report(book_path, word_count, character_count)
-    
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
 
 def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+    try:
+        with open(path) as f:
+            return f.read()
+    except FileNotFoundError:
+        print (f"Error: The file at '{path}' was not found")
+        sys.exit(1)
 
 
-def get_word_count(text):
-    words = text.split()
-    return len(words)
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
-def get_character_count(text):
-    count = {}
-    lowered = text.lower()
-    for c in lowered:
-        if c not in count:
-            count[c] = 1
-        else:
-            count[c] += 1
-    return count
-    
-
-def get_letter_report(book_path, word_count, character_count):
-    letters = []
-    for character in character_count:
-        count = character_count[character]
-        if character.isalpha():
-            letters.append((character, count))
-    letters.sort(reverse=True, key=lambda x: x[1])   
-    
-    print (f"--- Begin report of {book_path} ---")  
-    print (f"{word_count} words found in the document")
-    print ()
-    for character, count in letters:
-        print(f"The '{character}' character was found {count} times")
-    print(f"--- End report ---")
-    
-    
 main()
-
-
